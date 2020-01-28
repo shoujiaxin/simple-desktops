debe0d29fa2d7d951c2747185f0c10b357565268
Jiaxin Shou <shoujiaxin@gmail.com>
2020-01-29 00:33:27 +0800

    Add a button to change wallpaper

--- SimpleDesktops/ViewControllers/PopoverViewController.swift
+++ SimpleDesktops/ViewControllers/PopoverViewController.swift
@@ -12,8 +12,16 @@ class PopoverViewController: NSViewController {
     @IBOutlet var imageView: NSImageView!
     @IBOutlet var refreshButton: RefreshButton!
     @IBOutlet var progressIndicator: NSProgressIndicator!
+    @IBOutlet var setWallpaperButton: NSButton!
 
     private var imageManager: ImageManager!
+
+    override func viewWillLayout() {
+        setWallpaperButton.layer?.borderColor = CGColor(gray: 0.5, alpha: 0.4)
+        setWallpaperButton.layer?.borderWidth = 1
+        setWallpaperButton.layer?.cornerRadius = 4
+        setWallpaperButton.layer?.backgroundColor = CGColor(gray: 0.6, alpha: 0.2)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +30,8 @@ class PopoverViewController: NSViewController {
 
         progressIndicator.appearance = NSAppearance(named: .aqua)
         progressIndicator.isHidden = true
+
+        setWallpaperButton.attributedTitle = NSMutableAttributedString(string: "Set as Wallpaper", attributes: [NSAttributedString.Key.foregroundColor: NSColor.textColor])
 
         imageManager = ImageManager()
         set(refreshing: true)
@@ -56,6 +66,8 @@ class PopoverViewController: NSViewController {
     @IBAction func historyButtonClicked(_: Any) {}
 
     @IBAction func settingsButtonClicked(_: Any) {}
+
+    @IBAction func setWallpaperButtonClicked(_: Any) {}
 
     private func set(refreshing: Bool) {
         if refreshing {
