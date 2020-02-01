e02308b8f2993727dd2602fbb3d8736774d20087
Jiaxin Shou <shoujiaxin@gmail.com>
2020-02-01 22:33:45 +0800

    Update class: Options

--- SimpleDesktops/Options.swift
+++ SimpleDesktops/Options.swift
@@ -11,37 +11,62 @@ import Foundation
 class Options {
     static let shared = Options()
 
+    enum ChangeInterval: UInt8 {
+        case everyMinute
+        case everyFiveMinutes
+        case everyFifteenMinutes
+        case everyHour
+        case everyDay
+
+        static func from(rawValue: Int) -> ChangeInterval {
+            switch rawValue {
+            case 0: return everyMinute
+            case 1: return everyFiveMinutes
+            case 2: return everyFifteenMinutes
+            case 3: return everyHour
+            case 4: return everyDay
+            default: return everyHour
+            }
+        }
+    }
+
     // Default options tuple
     static let defaultOptions = (
         simpleDesktopsMaxPage: 51, // Max page of Simple Desktops
-        refreshInterval: 3600
+        changePicture: false,
+        changeInterval: ChangeInterval.everyHour
     )
 
     struct OptionsInfo {
         static let simpleDesktopsMaxPage = "SimpleDesktopsMaxPage"
-        static let refreshInterval = "RefreshInterval"
+        static let changePicture = "AutoPictureChange"
+        static let changeInterval = "PictureChangeInterval"
     }
 
     var simpleDesktopsMaxPage = Options.defaultOptions.simpleDesktopsMaxPage
-    var refreshInterval = Options.defaultOptions.refreshInterval
+    var changePicture = Options.defaultOptions.changePicture
+    var changeInterval = Options.defaultOptions.changeInterval
 
     func loadOptions() {
         let preferences = UserDefaults.standard
         if preferences.integer(forKey: OptionsInfo.simpleDesktopsMaxPage) == 0 {
             simpleDesktopsMaxPage = Options.defaultOptions.simpleDesktopsMaxPage
-            refreshInterval = Options.defaultOptions.refreshInterval
+            changePicture = Options.defaultOptions.changePicture
+            changeInterval = Options.defaultOptions.changeInterval
             saveOptions()
             return
         }
 
         simpleDesktopsMaxPage = preferences.integer(forKey: OptionsInfo.simpleDesktopsMaxPage)
-        refreshInterval = preferences.integer(forKey: OptionsInfo.refreshInterval)
+        changePicture = preferences.bool(forKey: OptionsInfo.changePicture)
+        changeInterval = ChangeInterval.from(rawValue: preferences.integer(forKey: OptionsInfo.changeInterval))
     }
 
     func saveOptions() {
         let preferences = UserDefaults.standard
 
         preferences.set(simpleDesktopsMaxPage, forKey: OptionsInfo.simpleDesktopsMaxPage)
-        preferences.set(refreshInterval, forKey: OptionsInfo.refreshInterval)
+        preferences.set(changePicture, forKey: OptionsInfo.changePicture)
+        preferences.set(changeInterval.rawValue, forKey: OptionsInfo.changeInterval)
     }
 }
--- SimpleDesktops/ViewControllers/PreferencesViewController.swift
+++ SimpleDesktops/ViewControllers/PreferencesViewController.swift
@@ -17,10 +17,21 @@ class PreferencesViewController: NSViewController {
         super.viewDidLoad()
 
         doneButton.attributedTitle = NSMutableAttributedString(string: "Done", attributes: [NSAttributedString.Key.foregroundColor: NSColor.textColor])
+
+        updatePreferences()
     }
 
     @IBAction func changePictureButtonClicked(_ sender: NSButton) {
-        intervalPopUpButton.isEnabled = (sender.state == NSControl.StateValue.on) ? true : false
+        switch sender.state {
+        case NSControl.StateValue.on:
+            Options.shared.changePicture = true
+            intervalPopUpButton.isEnabled = true
+        case NSControl.StateValue.off:
+            Options.shared.changePicture = false
+            intervalPopUpButton.isEnabled = false
+        default:
+            return
+        }
     }
 
     @IBAction func doneButtonClicked(_: Any) {
@@ -28,7 +39,17 @@ class PreferencesViewController: NSViewController {
         viewController.preferencesButtonClicked(self)
     }
 
+    @IBAction func intervalChanged(_ sender: NSPopUpButton) {
+        Options.shared.changeInterval = Options.ChangeInterval.from(rawValue: sender.indexOfSelectedItem)
+    }
+
     @IBAction func quitButtonClicked(_: Any) {
         NSApp.terminate(self)
+    }
+
+    private func updatePreferences() {
+        changePictureButton.state = Options.shared.changePicture ? NSControl.StateValue.on : NSControl.StateValue.off
+        intervalPopUpButton.selectItem(at: Int(Options.shared.changeInterval.rawValue))
+        intervalPopUpButton.isEnabled = Options.shared.changePicture
     }
 }
