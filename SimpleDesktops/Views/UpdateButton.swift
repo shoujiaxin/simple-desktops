78a3f67f214cafef7ff3a693a7d21cffef25e05b
Jiaxin Shou <shoujiaxin@gmail.com>
2020-05-30 00:43:28 +0800

    fix: UI bug of the refresh button

--- SimpleDesktops/Views/UpdateButton.swift
+++ SimpleDesktops/Views/UpdateButton.swift
@@ -9,16 +9,14 @@
 import Cocoa
 
 class UpdateButton: NSButton {
-    private var rectColor = NSColor.windowBackgroundColor.cgColor
-
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
 
-        rectColor = NSColor.windowBackgroundColor.cgColor
-
-        layer?.backgroundColor = CGColor(red: rectColor.components![0], green: rectColor.components![1], blue: rectColor.components![2], alpha: 0.2)
+        layer?.backgroundColor = NSColor.windowBackgroundColor.cgColor
         layer?.borderWidth = 0
         layer?.cornerRadius = 6
+
+        alphaValue = 0.3
 
         let rect = NSRect(x: bounds.origin.x - bounds.width, y: bounds.origin.y - bounds.height, width: bounds.width * 3, height: bounds.height * 3)
         let trackingArea = NSTrackingArea(rect: rect, options: [.mouseEnteredAndExited, .activeAlways], owner: self, userInfo: nil)
@@ -39,13 +37,13 @@ class UpdateButton: NSButton {
     override func mouseEntered(with event: NSEvent) {
         super.mouseEntered(with: event)
 
-        layer?.backgroundColor = CGColor(red: rectColor.components![0], green: rectColor.components![1], blue: rectColor.components![2], alpha: 0.8)
+        alphaValue = 0.8
     }
 
     override func mouseExited(with event: NSEvent) {
         super.mouseExited(with: event)
 
-        layer?.backgroundColor = CGColor(red: rectColor.components![0], green: rectColor.components![1], blue: rectColor.components![2], alpha: 0.2)
+        alphaValue = 0.3
     }
 }
 
