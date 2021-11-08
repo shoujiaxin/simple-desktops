55196beccff341a8bc910767dc1bfbb2c3b3a969
Jiaxin Shou <shoujiaxin@gmail.com>
2021-11-08 23:51:48 +0800

    refactor: update Core Data model

--- SimpleDesktops/ViewModels/Picture.swift
+++ SimpleDesktops/ViewModels/Picture.swift
@@ -8,6 +8,11 @@
 import CoreData
 
 extension Picture {
+    /// Construct a description of search criteria used to retrieve pictures from Core Data.
+    /// - Parameters:
+    ///   - predicate: The predicate of the fetch request.
+    ///   - fetchLimit: The fetch limit of the fetch request.
+    /// - Returns: An instance of [NSFetchRequest](https://developer.apple.com/documentation/coredata/nsfetchrequest).
     static func fetchRequest(_ predicate: NSPredicate?,
                              fetchLimit: Int = 0) -> NSFetchRequest<Picture> {
         let request = fetchRequest()
@@ -19,29 +24,37 @@ extension Picture {
         return request
     }
 
-    static func withURL(_ url: URL, in context: NSManagedObjectContext) -> Picture {
+    /// Retrieve the first matched picture with the specified value of the key path.
+    /// - Returns: Retrieved object or `nil` if it does not exist.
+    static func retrieveFirst<Value, Key>(
+        with value: Value,
+        for keyPath: KeyPath<Picture, Key>,
+        in context: NSManagedObjectContext
+    ) -> Picture? where Value: CVarArg {
         let request = fetchRequest(
-            NSPredicate(format: "url_ = %@", url.absoluteString),
+            NSPredicate(format: "%K == %@", NSExpression(forKeyPath: keyPath).keyPath, value),
             fetchLimit: 1
         )
-        if let picture = try? context.fetch(request).first {
-            return picture
-        } else {
-            let picture = Picture(context: context)
-            picture.url = url
-            return picture
-        }
+        return try? context.fetch(request).first
     }
 
+    /// Update an existing picture, or create one with the given SDPictureInfo if it does not exist. The url of the picture is used as the key for database queries.
+    /// - Parameters:
+    ///   - info: Information of the picture.
+    ///   - context: Managed object context of the persistent container.
+    /// - Returns: The updated object.
     @discardableResult
     static func update(with info: SDPictureInfo, in context: NSManagedObjectContext) -> Picture {
-        let picture = withURL(info.url, in: context)
-        picture.lastFetchedTime = Date()
+        let picture = retrieveFirst(with: info.url.absoluteString, for: \.url_, in: context) ??
+            Picture(context: context)
+
         if picture.id_ == nil {
             picture.id_ = UUID()
-            picture.name = info.name
-            picture.previewURL = info.previewURL
+            picture.url_ = info.url
         }
+        picture.lastFetchedTime = Date()
+        picture.name = info.name
+        picture.previewURL = info.previewURL
 
         try? context.save()
         return picture
--- SimpleDesktopsTests/PictureTests.swift
+++ SimpleDesktopsTests/PictureTests.swift
@@ -12,59 +12,65 @@ import XCTest
 class PictureTests: XCTestCase {
     private let context = PersistenceController.preview.container.viewContext
 
-    func testFetchPictureWithURL() throws {
+    func testRetrieveExisting() throws {
         let url =
             URL(
                 string: "http://static.simpledesktops.com/uploads/desktops/2020/06/28/Big_Sur_Simple.png"
             )!
-        let picture = Picture.withURL(url, in: context)
+        let picture = Picture.retrieveFirst(with: url.absoluteString, for: \.url_, in: context)
 
-        XCTAssertNotNil(picture.id_)
-        XCTAssertNotNil(picture.lastFetchedTime_)
-        XCTAssertEqual(picture.name, "2020-06-28-Big_Sur_Simple.png")
+        XCTAssertNotNil(picture?.id_)
+        XCTAssertNotNil(picture?.lastFetchedTime_)
+        XCTAssertEqual(picture?.name, "2020-06-28-Big_Sur_Simple.png")
         XCTAssertEqual(
-            picture.previewURL,
+            picture?.previewURL,
             URL(
                 string: "http://static.simpledesktops.com/uploads/desktops/2020/06/28/Big_Sur_Simple.png.625x385_q100.png"
             )
         )
-        XCTAssertEqual(picture.url, url)
+        XCTAssertEqual(picture?.url, url)
     }
 
-    func testCreatePictureWithURL() throws {
+    func testRetrieveNonExistent() throws {
         let url =
             URL(
                 string: "http://static.simpledesktops.com/uploads/desktops/2021/02/04/mirage.png.295x184_q100.png"
             )!
-        let picture = Picture.withURL(url, in: context)
+        let picture = Picture.retrieveFirst(with: url.absoluteString, for: \.url_, in: context)
 
-        XCTAssertNil(picture.id_)
-        XCTAssertNil(picture.lastFetchedTime_)
-        XCTAssertNil(picture.name)
-        XCTAssertNil(picture.previewURL)
-        XCTAssertEqual(picture.url, url)
+        XCTAssertNil(picture)
     }
 
     func testUpdate() throws {
         let link =
             "http://static.simpledesktops.com/uploads/desktops/2020/06/28/Big_Sur_Simple.png.625x385_q100.png"
         let info = SDPictureInfo(from: link)!
-        let pictureBefore = Picture.withURL(info.url, in: context)
-        let date = pictureBefore.lastFetchedTime
+        let pictureBefore = Picture.retrieveFirst(
+            with: info.url.absoluteString,
+            for: \.url_,
+            in: context
+        )
 
-        XCTAssertNotNil(pictureBefore.id_)
-        XCTAssertNotNil(pictureBefore.lastFetchedTime_)
-        XCTAssertEqual(pictureBefore.name, info.name)
-        XCTAssertEqual(pictureBefore.previewURL, info.previewURL)
-        XCTAssertEqual(pictureBefore.url, info.url)
+        XCTAssertNotNil(pictureBefore?.id_)
+        XCTAssertNotNil(pictureBefore?.lastFetchedTime_)
+        XCTAssertEqual(pictureBefore?.name, info.name)
+        XCTAssertEqual(pictureBefore?.previewURL, info.previewURL)
+        XCTAssertEqual(pictureBefore?.url, info.url)
 
-        let pictureAfter = Picture.update(with: info, in: context)
+        let lastFetchedTime = pictureBefore?.lastFetchedTime
+        let name = pictureBefore?.name
 
+        let newInfo = SDPictureInfo(name: UUID().uuidString, previewURL: info.url, url: info.url)
+        let pictureAfter = Picture.update(with: newInfo, in: context)
+
+        XCTAssertEqual(pictureAfter, pictureBefore)
         XCTAssertNotNil(pictureAfter.id_)
-        XCTAssertNotEqual(date, pictureAfter.lastFetchedTime)
-        XCTAssertEqual(pictureAfter.name, info.name)
-        XCTAssertEqual(pictureAfter.previewURL, info.previewURL)
-        XCTAssertEqual(pictureAfter.url, info.url)
+        XCTAssertNotEqual(pictureAfter.lastFetchedTime, lastFetchedTime)
+        XCTAssertEqual(pictureAfter.name, newInfo.name)
+        XCTAssertNotEqual(pictureAfter.name, name)
+        XCTAssertEqual(pictureAfter.previewURL, newInfo.previewURL)
+        XCTAssertEqual(pictureAfter.previewURL, info.url)
+        XCTAssertEqual(pictureAfter.url, newInfo.url)
     }
 
     func testAdd() throws {
